eb06ed453e85baa6164779d54d566748f20eb9e9
clop <clop@local>
2026-02-21 11:38:48 +0200

    ui: show your total VP bottom-right

--- src/multiplayer/SpineUI.jsx
+++ src/multiplayer/SpineUI.jsx
@@ -679,6 +679,13 @@ function Board({ G, ctx, moves, playerID }) {
         })()}
       </div>
 
+      {/* Total VP (bottom-right) */}
+      <div className="fixed bottom-4 right-4 z-[2500] pointer-events-none select-none">
+        <div className="bg-black/60 border border-amber-900/20 rounded-full px-4 py-2 text-amber-100/90 font-mono font-black tracking-widest text-[14px]">
+          VP: {myCoalitionPoints}
+        </div>
+      </div>
+
       {/* Hand fan */}
       <div className="fixed bottom-6 right-[310px] z-[999] pointer-events-auto">
         <div
