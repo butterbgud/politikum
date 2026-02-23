bfc063aa152258eda6574137485f5d60f17a1435
clop <clop@local>
2026-02-23 17:27:56 +0200

    politikum ui: clear game-over fan hover on pointerout/mouseleave

--- src/multiplayer/SpineUI.jsx
+++ src/multiplayer/SpineUI.jsx
@@ -1597,6 +1597,8 @@ function ActionBoard({ G, ctx, moves, playerID }) {
                       onPointerEnter={() => setHoverOppCoalition((m) => ({ ...(m || {}), [`go-${pid}`]: 0 }))}
                       onPointerLeave={() => setHoverOppCoalition((m) => ({ ...(m || {}), [`go-${pid}`]: null }))}
                       onPointerCancel={() => setHoverOppCoalition((m) => ({ ...(m || {}), [`go-${pid}`]: null }))}
+                      onPointerOut={() => setHoverOppCoalition((m) => ({ ...(m || {}), [`go-${pid}`]: null }))}
+                      onMouseLeave={() => setHoverOppCoalition((m) => ({ ...(m || {}), [`go-${pid}`]: null }))}
                     >
                       {coal.slice(0, show).map((c, i) => {
                         const t = show <= 1 ? 0.5 : i / (show - 1);
