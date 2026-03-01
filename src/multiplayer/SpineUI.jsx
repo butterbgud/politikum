6967ae73c614a3fb574aff9fb461b1cb055304b7
clop <clop@local>
2026-03-01 18:04:32 +0200

    Fix: import useRef (white screen)

--- src/multiplayer/SpineUI.jsx
+++ src/multiplayer/SpineUI.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Client } from 'boardgame.io/react';
 import { SocketIO } from 'boardgame.io/multiplayer';
 import { LobbyClient } from 'boardgame.io/client';
